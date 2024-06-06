bf68d56c6ed1d091ab45de69d1e12e6e33ae783b
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2024-06-06 19:32:21 -0300

    enemigo sigue a jugador

---
+++ Castigo Divino/Assets/Scripts/FollowAI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAI : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform player;
+    private bool isFacingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+        bool isPlayerRight = transform.position.x < player.transform.position.x;
+        Flip(isPlayerRight);
+    }
+
+    private void Flip(bool isPlayerRight)
+    {
+        if((isFacingRight && !isPlayerRight) || (!isFacingRight && !isPlayerRight))
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale; 
+        }
+    }
+}
