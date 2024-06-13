86c5d111e7f2a67dd3b18613bccd182b30aeac98
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2024-06-13 11:31:00 -0300

    flujo menu-juego

---
+++ Castigo Divino/Assets/Scripts/SceneFlow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class SceneFlow : MonoBehaviour
+{
+    [SerializeField] private bool pasarNivel;
+    [SerializeField] private int indiceNivel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CambiarNivel(indiceNivel);
+        }
+
+        if (pasarNivel)
+        {
+            CambiarNivel(indiceNivel);
+        }
+    }
+
+    public void CambiarNivel(int indice)
+    {
+        SceneManager.LoadScene(indice);
+    }
+}
