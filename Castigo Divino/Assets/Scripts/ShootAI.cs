af000c9f65c950c8f43b5acf7f09636ce049a1bf
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2024-06-28 14:17:01 -0300

    disparo arreglado

--- Castigo Divino/Assets/Scripts/ShootAI.cs
+++ Castigo Divino/Assets/Scripts/ShootAI.cs
@@ -1,25 +1,41 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShootAI : MonoBehaviour
 {
     [SerializeField] private GameObject proyectilePrefab;
-    [SerializeField] private float timeBetweenShoots;
-    void Start()
-    {
-        StartCoroutine(Shoot());
-    }
-    IEnumerator Shoot()
-    {
-        while (true) 
-        {
-            yield return new WaitForSeconds (timeBetweenShoots);
-            Instantiate (proyectilePrefab, transform.position, Quaternion.identity);
+    [SerializeField] private Transform player;
+    [SerializeField] private float shootDistance = 10f;
 
+    private bool isShooting;
+    private bool inRange;
+
+    void Update()
+    {
+        float distanceToTarget = Vector2.Distance(transform.position, player.position);
+        if (distanceToTarget < shootDistance)
+        {
+            if (!isShooting)
+            {
+                isShooting = true;
+                inRange = true;
+                StartCoroutine(Shoot());
+            }
+        }
+        else
+        {
+            inRange = false;
         }
     }
+
+    IEnumerator Shoot()
+    {
+        while (inRange)
+        {
+            Debug.Log("Shooting at target...");
+            Instantiate(proyectilePrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+        }
+        isShooting = false;
+    }
 }
-
-
