7ec9a3d7c6f85c7f1a2c8daf8c219cbe80508756
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2024-06-29 00:39:46 -0300

    movimiento personaje

--- Castigo Divino/Assets/Scripts/PlayerMovement.cs
+++ Castigo Divino/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,18 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D playerRb;
     private Vector2 moveInput;
     private Vector3 originalScale;
+    private SpriteRenderer spriteRenderer;
 
     [Header("Dash Settings")]
     [SerializeField] float dashSpeed = 25f;
     [SerializeField] float dashDuration = 0.25f;
     [SerializeField] float dashCoolDown = 0.7f;
+
+    [Header("Sprites")]
+    [SerializeField] private Sprite defaultSprite; // Sprite por defecto
+    [SerializeField] private Sprite backSprite; // Sprite "sin brazos atras"
+    [SerializeField] private Sprite sidesSprite;
+    [SerializeField] private Sprite sidesSprites;
 
     bool isDashing;
     bool canDash;
@@ -23,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody2D>();
         originalScale = transform.localScale;
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Inicializar el SpriteRenderer
         canDash = true;
     }
 
@@ -38,8 +46,26 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
         moveInput = new Vector2(moveX, moveY).normalized;
 
-        // Detect mouse position relative to player position
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Cambiar sprite si se presiona la tecla "arriba" o "W"
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            spriteRenderer.sprite = backSprite;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            spriteRenderer.sprite = sidesSprite;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            spriteRenderer.sprite = sidesSprites;
+        }
+        else
+        {
+            spriteRenderer.sprite = defaultSprite;
+        }
+
+        // Detectar posición del ratón relativa a la posición del jugador
+        /*Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (mousePosition.x < transform.position.x)
         {
             transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
@@ -49,7 +75,7 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
             IsFacingRight = true;
-        }
+        }*/
 
         if (Input.GetKeyDown(KeyCode.Space) && canDash)
         {
@@ -74,7 +100,6 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log("dashing");
         canDash = false;
         isDashing = true;
-        //playerRb.velocity = new Vector2 (moveInput.x * dashSpeed, moveInput.y * dashSpeed);
         yield return new WaitForSeconds(dashDuration);
         isDashing = false;
 
