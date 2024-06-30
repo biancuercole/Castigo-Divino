0a28aa2499acc1366a7ccad353833d3c06f421c9
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2024-06-30 02:04:40 -0300

    aviso inicial

--- Castigo Divino/Assets/Scripts/NextStage.cs
+++ Castigo Divino/Assets/Scripts/NextStage.cs
@@ -9,6 +9,16 @@ public class NextStage : MonoBehaviour
     [SerializeField] private int enemyCount; // Total de enemigos en la escena
     [SerializeField] private int levelIndex;
 
+    [Header("Sprites")]
+    [SerializeField] private Sprite closedSprite; // Sprite cerrada
+    [SerializeField] private Sprite openSprite; // sprite abierta
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Inicializar el SpriteRenderer
+    }
+
     // Método para reducir el contador de enemigos
     public void EnemyDefeated()
     {
@@ -16,11 +26,21 @@ public class NextStage : MonoBehaviour
         Debug.Log("Enemigos restantes: " + enemyCount);
     }
 
+    void Update()
+    {
+        if(enemyCount == 0)
+        {
+            spriteRenderer.sprite = openSprite;
+        } else
+        {
+            spriteRenderer.sprite = closedSprite;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D Player)
     {
         if(enemyCount == 0)
         {
-        changeLevel(levelIndex);
+            changeLevel(levelIndex);
         }
     }
 
