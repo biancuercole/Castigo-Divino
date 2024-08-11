c3c769f2e5548018156ec8effcba6a60cc1534f1
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2024-08-11 01:13:59 -0300

    cooldown arma

--- Castigo Divino/Assets/Scripts/Rotation.cs
+++ Castigo Divino/Assets/Scripts/Rotation.cs
@@ -1,39 +1,37 @@
-//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    
     [SerializeField] private float rotationSpeed;
     [SerializeField] private Transform shootPosition;
-
-
+    [SerializeField] private float shootCooldown = 1f; // Cooldown de 3 segundos
 
     private Camera cam;
+    private float lastShootTime; // Tiempo del último disparo
+
     void Start()
     {
         cam = Camera.main;
+        lastShootTime = -shootCooldown; // Permite disparar inmediatamente al iniciar
     }
-
 
     void Update()
     {
         Vector2 mouseWorldPoint = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = mouseWorldPoint - (Vector2)transform.position;
 
-        // Calcular el angulo y ajustar la rotacion
+        // Calcular el ángulo y ajustar la rotación
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 180f;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
-
-        if (Input.GetMouseButtonDown(0))
+        // Verificar si el tiempo actual es mayor al último tiempo de disparo más el cooldown
+        if (Input.GetMouseButtonDown(0) && Time.time >= lastShootTime + shootCooldown)
         {
-
+            lastShootTime = Time.time; // Actualizar el tiempo del último disparo
 
             GameObject bullet = BulletPool.Instance.RequestBullet();
             if (bullet != null)
             {
-             // Debug.Log("Bullet activated, setting position and rotation");
                 bullet.transform.position = shootPosition.position;
                 bullet.transform.rotation = Quaternion.Euler(0, 0, angle);
                 bullet.GetComponent<Bullets>().LaunchBullet(direction);
