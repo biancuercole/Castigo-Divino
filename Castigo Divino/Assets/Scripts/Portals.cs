3e71ea5be75ebc4ba14805b5d0b45ff4485d5458
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2024-09-04 17:05:23 -0300

    portales

--- Castigo Divino/Assets/Scripts/Portals.cs
+++ Castigo Divino/Assets/Scripts/Portals.cs
@@ -6,30 +6,42 @@ using UnityEngine.SceneManagement;
 public class Portals : MonoBehaviour
 {
     private PlayerMovement player;
-    private Collider2D portalCollider;
     [SerializeField] private int level;
     private TransicionEscena transition;
+    
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite closedSprite;
+    [SerializeField] private Sprite openSprite;
+    private Collider2D portalCollider;
 
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        portalCollider = GetComponent<Collider2D>();
+
+        spriteRenderer.sprite = closedSprite;
+        portalCollider.enabled = false;
+
         transition = FindObjectOfType<TransicionEscena>();
         player = FindObjectOfType<PlayerMovement>();
 
         // Obtén el collider del portal
         portalCollider = GetComponent<Collider2D>();
 
-        // Desactiva el portal (incluye el collider y el renderer)
-        gameObject.SetActive(false);
+        /* Desactiva el portal (incluye el collider y el renderer)
+        gameObject.SetActive(false);*/
         Debug.Log("Portal inactivo al inicio");
     }
 
     public void EnablePortal()
     {
-        // Activa el portal (incluye el collider y el renderer)
-        gameObject.SetActive(true);
+        /* Activa el portal (incluye el collider y el renderer)
+        gameObject.SetActive(true);*/
 
         // Activa el collider y cambia la capa
+
         portalCollider.enabled = true;
+        spriteRenderer.sprite = openSprite;
         gameObject.layer = 3; // Cambia "3" por la capa que desees
         Debug.Log("Portal activado y capa cambiada");
     }
