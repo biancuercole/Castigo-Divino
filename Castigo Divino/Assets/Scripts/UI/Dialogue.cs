fcde0115ebf92447dd90ccd0b1936b3357aa708a
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2024-09-09 18:33:15 -0300

    dialogos terminados

--- Castigo Divino/Assets/Scripts/UI/Dialogue.cs
+++ Castigo Divino/Assets/Scripts/UI/Dialogue.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
     private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
+    private float typingTime = 0.05f;
+    [SerializeField] private GameObject dialogueStart;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+
     void Update()
     {
-        
+        if(isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            if(!didDialogueStart)
+            {
+                StartDialogue();
+            } else if (dialogueText.text == dialogueLines[lineIndex])
+            {
+                nextDialogueLine();
+            }else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[lineIndex];
+            }
+        }
     }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        dialogueStart.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(showLine());
+    }
+
+    private void nextDialogueLine()
+    {
+        lineIndex++;
+        if(lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(showLine());
+        } else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            dialogueStart.SetActive(true);
+            Time.timeScale = 1f;
+        }
+    }
+
+    private IEnumerator showLine()
+    {
+        dialogueText.text = string.Empty;
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = true;
+            dialogueStart.SetActive(true);
         }
     }
 
@@ -21,6 +80,7 @@ public class Dialogue : MonoBehaviour
         if(collision.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = false;
+            dialogueStart.SetActive(false);
         }
     }
 }
