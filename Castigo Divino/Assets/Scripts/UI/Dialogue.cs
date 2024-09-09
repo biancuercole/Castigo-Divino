c846b758220c2f702dc17dbccb7d4a8ebc991ff0
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2024-09-09 03:29:10 -0300

    prueba dialogo

---
+++ Castigo Divino/Assets/Scripts/UI/Dialogue.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    private bool isPlayerInRange;
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+        }
+    }
+}
